5a7bb6c7ef6d0873a8e146f4b3f16655067e3c6b
Kirill Budkevich <byt1kip82@gmail.com>
2024-07-07 19:23:17 +0300

    fix vertical swipes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Tetris from './components/Tetris/Tetris'
 import Home from './components/Home/Home'
 
-
 const App = () => {
+  const tg = window.Telegram.WebApp
+
+  useEffect(() => {
+    tg.ready()
+    tg.expand()
+    tg.disableVerticalSwipes()
+  }, [])
+
+
   return (
     <BrowserRouter>
       <Routes>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import logo from '../../assets/logo.png'
 import s from './Home.module.css'
 import { NavLink } from 'react-router-dom'
 
 const Home = () => {
+  const tg = window.Telegram.WebApp
+
+  const onClone = () => {
+    tg.close()
+  }
+
   return (
       <div className={'container ' + s.container}>
         <img src={logo} className={s.logo} alt="tetris" />
-        <h1>Hello, {`{username}`}</h1>
+        <h1>Hello, {`${tg.initDataUnsafe.user.first_name}`}</h1>
         <p className={s.text}>Tetris is a puzzle video game created in 1985 by Alexey Pajitnov, a Soviet software engineer.</p>
         <NavLink to='/game' className={s.btn}>Start Game</NavLink>
+        <button className={s.close} onClick={() => onClone()}>Exit</button>
         <p className={s.credits}>Spizheno by the best developers :)</p>
       </div>
   )
