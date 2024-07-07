311bc6fea8c920ef68462201f35e1e728f4f8706
Kirill Budkevich <byt1kip82@gmail.com>
2024-07-07 13:56:28 +0300

    fix deploy

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
       <div className={'container ' + s.container}>
         <img src={logo} className={s.logo} alt="tetris" />
-        <h1>Hello, {`${'{username}'}`}</h1>
+        <h1>Hello, {`{username}`}</h1>
         <p className={s.text}>Tetris is a puzzle video game created in 1985 by Alexey Pajitnov, a Soviet software engineer.</p>
         <NavLink to='/game' className={s.btn}>Start Game</NavLink>
         <p className={s.credits}>Spizheno by the best developers :)</p>
