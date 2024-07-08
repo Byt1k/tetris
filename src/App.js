c4f8898509dc088eb80e059249fe13f25f56f9d1
Kirill Budkevich <byt1kip82@gmail.com>
2024-07-08 12:15:37 +0300

    add disableVerticalSwipes

--- src/App.js
+++ src/App.js
@@ -5,15 +5,12 @@ import Tetris from './components/Tetris/Tetris'
 import Home from './components/Home/Home'
 
 const App = () => {
-  const tg = {
-    ...window.Telegram.WebApp,
-    isVerticalSwipesEnabled: false
-  }
+  const tg = window.Telegram.WebApp
 
   useEffect(() => {
     tg.ready()
     tg.expand()
-    // tg.disableVerticalSwipes()
+    tg.disableVerticalSwipes()
   }, [])
 
 
